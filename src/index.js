ae0a9f70763f3dde240cfbfdb0fbb3ac7fdb296d
Maíra Bassetto <mairabassetto@gmail.com>
2021-10-03 13:40:38 -0300

    Solve exercices about middlewares and coverage all tests suits

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({error: "User not found!"});
+  };
+
+  request.user = user;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
-}
+  const { user } = request;
+
+  if(user.todos.length >= 10 && !user.pro) {
+    return response.status(403).json({error: "You have more than 10 todos!"})
+  };
+ 
+  return next();
+};
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { id: todoId } = request.params;
+  const { username } = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user) {
+    return response.status(404).json({ error: "User not found"});
+  }
+
+  if(!validate(todoId)) {
+    return response.status(400).json({ error: "UUID is not valid!"});
+  }
+
+  const todo = user.todos.find(todo => todo.id === todoId);
+
+  if(!todo) {
+    return response.status(404).json({ error: "Todo not found!"});
+  }
+
+  request.user = user;
+  request.todo = todo;
+
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user) {
+    return response.status(404).json({ error: "User not found!"});
+  }
+
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
